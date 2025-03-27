317b5a1e05c963dfb057c2440d2f8295f24a778d
Suraj Sedai <surajseda05@gmail.com>
2025-03-27 12:31:54 -0400

    process the image

--- src/text_detection.py
+++ src/text_detection.py
@@ -2,6 +2,8 @@ import cv2
 import os
 import pytesseract
 import easyocr
+import pytesseract
+from PIL import Image
 
 # Ensure output directory exists
 os.makedirs("frames", exist_ok=True)
@@ -24,7 +26,25 @@ else:
     video.release()
     print(f"Extracted {count} frames successfully.")
 
-# OCR Processing on First Frame
+
+
+image = cv2.imread("frames/frame_86.jpg")
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+# Save preprocessed image for debugging (optional)
+cv2.imwrite("processed.png", gray)
+
+# Perform OCR using pytesseract
+text = pytesseract.image_to_string(gray)
+
+print("Extracted Text:")
+print(text)
+
+
+'''# OCR Processing on First Frame
 image_path = "frames/frame_86.jpg"
 image = cv2.imread(image_path)
 
@@ -39,7 +59,7 @@ print("Detected Text:", text)
 # EasyOCR
 reader = easyocr.Reader(['en'])
 results = reader.readtext(image)
-print("EasyOCR Results:", results)
+print("EasyOCR Results:", results)'''
 
 
 # from googletrans import Translator
